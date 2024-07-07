a4797651917e92a24717159500f4ac5a5ba91877
jochongs <joch2712@naver.com>
2024-07-08 01:05:35 +0900

    feat: Add User model

--- src/main/java/org/example/builder/user/model/User.java
+++ src/main/java/org/example/builder/user/model/User.java
@@ -1,2 +1,40 @@
-package org.example.builder.user.model;public class User {
+package org.example.builder.user.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "user_tb")
+@Getter
+@Setter
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    @Size(min = 2, max = 8)
+    private String name;
+
+    @NotNull
+    @Column(name = "email", nullable = false)
+    @Email
+    private String email;
+
+    @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 }
