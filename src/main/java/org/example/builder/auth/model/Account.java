6e12e1461d704416cc6438ef9b22c6d461b2440e
jochongs <joch2712@naver.com>
2024-07-08 01:10:24 +0900

    feat: Add Account model

--- src/main/java/org/example/builder/auth/model/Account.java
+++ src/main/java/org/example/builder/auth/model/Account.java
@@ -1,2 +1,28 @@
-package org.example.builder.auth.model;public class Account {
+package org.example.builder.auth.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.example.builder.user.model.User;
+
+@Entity(name = "account_tb")
+@Getter
+@Setter
+@ToString
+public class Account {
+
+    @Id
+    @Column(name = "idx")
+    private Long idx;
+
+    @NotNull
+    @Column(name = "pw", nullable = false)
+    @NotNull
+    private String pw;
+
+    @OneToOne
+    @JoinColumn(name = "idx", referencedColumnName = "idx", insertable = false, updatable = false)
+    User user;
 }
